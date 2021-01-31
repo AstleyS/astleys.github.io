2f4d17b428ac8b243927b905b66a1ac0f0466ecc
Astley Santos <astley_394@hotmail.coml>
2021-01-31 01:24:36 +0100

    Added a function to remove the menu after clicked on a certain item/link Next, complete the about and contact sections

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ const showMenu = (navS, navLinksS, burgerS) => {
 }
 
 showMenu('nav', 'nav_links', 'burger');
+/******* END SHOW MENU *******/
+
+/******* HIDE MENU ONCLICK ITEM *******/
+function hide_menu () {
+    const nav = document.getElementById('nav')
+    const navLinks = document.getElementById('nav_links')
+    if (nav && navLinks) {
+        nav.classList.remove('show-menu');
+        navLinks.classList.remove('show-menu-links');
+    }
+}
+
+// This variable will contain all items of the nav-links
+const navItems = document.querySelectorAll('.nav-item');
+// If any items was clicked, then the menu will be removed
+navItems.forEach(item => item.addEventListener('click', hide_menu));
+/******* END HIDE MENU *******/
