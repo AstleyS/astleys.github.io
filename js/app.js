02cd2335b607090480542cc7b849783df65402bd
Astley Santos <astley_394@hotmail.coml>
2021-02-01 02:10:26 +0100

    Added a js function responsible to change the state of active link Next, validate the contact form and make the skill section

--- js/app.js
+++ js/app.js
@@ -37,3 +37,21 @@ navItems.forEach(item => item.addEventListener('click', hide_menu));
 /******* END HIDE MENU *******/
 
 /******* SCROLL SECTIONS ACTIVE LINK *******/
+function activeLink() {
+    // This variable will contain all sections of the portfolio
+    const sections = document.querySelectorAll('section[id]')
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', activeLink);
