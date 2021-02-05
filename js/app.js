ca019b214ffb1e9b7fa4814bde264aeb6e7a8550
Astley Santos <astley_394@hotmail.coml>
2021-02-05 23:02:19 +0100

    Commented some js code and made some changes on about section content (hobbies => skills)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+// Calling showMenu function with nav, nav_links and burger id names as args
+showMenu('nav', 'nav_links', 'burger');
+
+// This variable will contain all items of the nav-links
+const navItems = document.querySelectorAll('.nav-item');
+// If any item was clicked, then the menu will be removed
+navItems.forEach(item => item.addEventListener('click', hide_menu));
+
+// The window will keep "listening" the position of the scroll to indicate the
+// active link
+window.addEventListener('scroll', activeLink);
+
+
 /******* SHOW MENU *******/
-const showMenu = (navS, navLinksS, burgerS) => {
+function showMenu(navS, navLinksS, burgerS) {
     // Find the selectors in html
     const nav = document.getElementById(navS);
     const navLinks = document.getElementById(navLinksS);
@@ -15,8 +28,6 @@ const showMenu = (navS, navLinksS, burgerS) => {
         });
     }
 }
-
-showMenu('nav', 'nav_links', 'burger');
 /******* END SHOW MENU *******/
 
 /******* HIDE MENU ONCLICK ITEM *******/
@@ -29,24 +40,24 @@ function hide_menu () {
         navLinks.classList.remove('show-menu-links');
     }
 }
-
-// This variable will contain all items of the nav-links
-const navItems = document.querySelectorAll('.nav-item');
-// If any item was clicked, then the menu will be removed
-navItems.forEach(item => item.addEventListener('click', hide_menu));
 /******* END HIDE MENU *******/
 
 /******* SCROLL SECTIONS ACTIVE LINK *******/
 function activeLink() {
-    // This variable will contain all sections of the portfolio
-    const sections = document.querySelectorAll('section[id]')
+    // This variable will contain all sections (that contains an id) of the portfolio
+    const sections = document.querySelectorAll('section[id]');
+    // Y position of scroll
     const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
+        // Get section height
         const sectionHeight = current.offsetHeight;
+        // Get the top position of section. -50 because of the header
         const sectionTop = current.offsetTop - 50;
+        // Get the id of the section
         let sectionId = current.getAttribute('id');
 
+        // Verifying in which section the scroll position is on 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.add('active-link');
         } else {
@@ -54,4 +65,3 @@ function activeLink() {
         }
     });
 }
-window.addEventListener('scroll', activeLink);
