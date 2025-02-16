9a3449c42c7ea1ff12713491ecac107bbf058c34
AstleyS <astley_394@hotmail.com>
2025-02-16 20:30:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import MaintenancePage from './components/MaintenancePage';
+import Header from './components/Header/Header'
 
 function App() {
-  const isMaintenanceMode = false; // Set to true to enable maintenance mode
-
-  if (isMaintenanceMode) {
-    return <MaintenancePage />;
-  }
 
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <Header />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
