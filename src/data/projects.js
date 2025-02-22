39162051d23dc1b0aaafcb5fa4b515e20b74eb87
AstleyS <astley_394@hotmail.com>
2025-02-22 01:24:47 +0100

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,21 @@
 import projectGif from '../assets/project_gif.gif';
-import projectImg from '../assets/upcoming_project.png';
+import projectImg from '../assets/project_img.png';
 
+import upcomingImg from '../assets/upcoming_project.png';
+import flaskTodoList from '../assets/flask_todolist.png';
+//import vanillaJsPortfolio from '../assets/vanilla_js_portfolio.png';
+import reactPortfolio from '../assets/react_portfolio.png';
+// import pathfinderVisualizer from '../assets/pathfinder_visualizer.png';
+// import violinPosture from '../assets/violin_posture.png';
+// import angularTodoList from '../assets/angular_todolist.png';
+// import inventoryManagement from '../assets/inventory_management.png';
 
 const projects = [
   {
     id: 1,
     title: 'Flask Todo List',
     technologies: ['Python', 'Flask'],
-    image: projectImg,
+    image: flaskTodoList,
     gif: projectGif,
     website: null,
     github: 'https://github.com/AstleyS/web-flask-todolist',
@@ -29,10 +37,10 @@ const projects = [
     id: 3,
     title: 'React Portfolio Website',
     technologies: ['react'],
-    image: projectImg,
+    image: reactPortfolio,
     gif: projectGif,
     website: '#',
-    github: 'https://github.com/AstleyS/astleys.github.io-react',
+    github: 'https://github.com/AstleyS/astleys.github.io',
     description: 'Astley\'s personal portfolio website, redesigned using React.js for improved modularity, performance, and user experience.',
     available: true
   },
@@ -40,7 +48,7 @@ const projects = [
     id: 4,
     title: 'Pathfinder Algorithm Visualizer',
     technologies: ['react'],
-    image: projectImg,
+    image: upcomingImg,
     gif: projectGif,
     website: 'https://astleys.github.io/web-pathfinder-visualizer',
     github: 'https://github.com/AstleyS/web-pathfinder-visualizer',
@@ -51,10 +59,10 @@ const projects = [
     id: 5,
     title: 'Violin Posture Correction',
     technologies: ['Python', 'AI/ML'],
-    image: projectImg,
+    image: upcomingImg,
     gif: projectGif,
     website: '#',
-    github: 'https://github.com/AstleyS',
+    github: 'https://github.com/AstleyS/ai-violin-posure-tutor',
     description: 'A Python and AI/ML-based project that detects and analyses the posture of violin and viola players, providing feedback and corrections to improve technique and ergonomics.',
     available: false
   },
@@ -62,10 +70,10 @@ const projects = [
     id: 6,
     title: 'Angular Todo List Application',
     technologies: ['angular'],
-    image: projectImg,
+    image: upcomingImg,
     gif: projectGif,
     website: '#',
-    github: 'https://github.com/AstleyS/ai-violin-posure-tutor',
+    github: 'https://github.com/AstleyS/web-angular-todolist',
     description: 'A simple and interactive Todo List application developed with Angular, allowing users to efficiently create, update, and delete tasks while maintaining a structured UI.',
     available: false
   },
@@ -73,7 +81,7 @@ const projects = [
     id: 7,
     title: 'Inventory Management System',
     technologies: ['spring boot', 'angular'],
-    image: projectImg,
+    image: upcomingImg,
     gif: projectGif,
     website: '#',
     github: 'https://github.com/AstleyS/inventory-management',
